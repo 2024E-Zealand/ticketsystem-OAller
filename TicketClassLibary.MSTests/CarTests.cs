5b0ebf9c661b79e91e4dbb2b4487fe53aa3f1940
Oliver Aller <ola001@edu.zealand.dk>
2024-10-04 12:08:54 +0200

    Tilføjet Car klasse og unittest

---
+++ TicketClassLibary.MSTests/CarTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicketClassLibrary.Model;
+using System;
+
+namespace TicketClassLibrary.MSTests
+{
+    [TestClass]
+    public class CarTests
+    {
+        [TestMethod]
+        public void Price_ShouldReturnFixedPrice()
+        {
+            // Arrange
+            var car = new Car();
+
+            // Act
+            double price = car.Price();
+
+            // Assert
+            Assert.AreEqual(240.0, price);
+        }
+
+        [TestMethod]
+        public void VehicleType_ShouldReturnCar()
+        {
+            // Arrange
+            var car = new Car();
+
+            // Act
+            string vehicleType = car.VehicleType();
+
+            // Assert
+            Assert.AreEqual("Car", vehicleType);
+        }
+
+        [TestMethod]
+        public void Licenseplate_ShouldStoreAndReturnLicenseplate()
+        {
+            // Arrange
+            var car = new Car();
+            string expectedLicenseplate = "AB12345";
+
+            // Act
+            car.Licenseplate = expectedLicenseplate;
+
+            // Assert
+            Assert.AreEqual(expectedLicenseplate, car.Licenseplate);
+        }
+
+        [TestMethod]
+        public void Date_ShouldStoreAndReturnDate()
+        {
+            // Arrange
+            var car = new Car();
+            DateTime expectedDate = new DateTime(2024, 10, 4);
+
+            // Act
+            car.Date = expectedDate;
+
+            // Assert
+            Assert.AreEqual(expectedDate, car.Date);
+        }
+    }
+}
---
+++ TicketClassLibrary/Model/Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TicketClassLibrary.Model
+{
+    /// <summary>
+    /// Repræsenterer en bil, som krydser broen.
+    /// </summary>
+    public class Car
+    {
+        /// <summary>
+        /// Nummerpladen på bilen.
+        /// </summary>
+        public string Licenseplate { get; set; }
+
+        /// <summary>
+        /// Dato for krydsning.
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Returnerer den faste pris for at krydse broen.
+        /// </summary>
+        /// <returns>Prisen som en double (240 DKK).</returns>
+        public double Price()
+        {
+            return 240.0;
+        }
+
+        /// <summary>
+        /// Returnerer køretøjstypen som en streng.
+        /// </summary>
+        /// <returns>En streng, der repræsenterer køretøjstypen.</returns>
+        public string VehicleType()
+        {
+            return "Car";
+        }
+    }
+}
