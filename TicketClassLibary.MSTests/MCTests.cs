6306ac289caf6ed1f57e89397044cb93b78820de
Oliver Aller <ola001@edu.zealand.dk>
2024-10-04 12:12:12 +0200

    Tilføjet MC klasse og unit tests

---
+++ TicketClassLibary.MSTests/MCTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicketClassLibrary.Model;
+using System;
+
+namespace TicketClassLibrary.MSTests
+{
+    [TestClass]
+    public class MCTests
+    {
+        [TestMethod]
+        public void Price_ShouldReturnFixedPrice()
+        {
+            // Arrange
+            var mc = new MC();
+
+            // Act
+            double price = mc.Price();
+
+            // Assert
+            Assert.AreEqual(125.0, price);
+        }
+
+        [TestMethod]
+        public void VehicleType_ShouldReturnMC()
+        {
+            // Arrange
+            var mc = new MC();
+
+            // Act
+            string vehicleType = mc.VehicleType();
+
+            // Assert
+            Assert.AreEqual("MC", vehicleType);
+        }
+
+        [TestMethod]
+        public void Licenseplate_ShouldStoreAndReturnLicenseplate()
+        {
+            // Arrange
+            var mc = new MC();
+            string expectedLicenseplate = "XY98765";
+
+            // Act
+            mc.Licenseplate = expectedLicenseplate;
+
+            // Assert
+            Assert.AreEqual(expectedLicenseplate, mc.Licenseplate);
+        }
+
+        [TestMethod]
+        public void Date_ShouldStoreAndReturnDate()
+        {
+            // Arrange
+            var mc = new MC();
+            DateTime expectedDate = new DateTime(2024, 10, 5);
+
+            // Act
+            mc.Date = expectedDate;
+
+            // Assert
+            Assert.AreEqual(expectedDate, mc.Date);
+        }
+    }
+}
--- TicketClassLibrary/Model/MC.cs
+++ TicketClassLibrary/Model/MC.cs
@@ -1,12 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TicketClassLibrary.Model
 {
-    internal class MC
+    /// <summary>
+    /// Repræsenterer en motorcykel (MC), som krydser broen.
+    /// </summary>
+    public class MC
     {
+        /// <summary>
+        /// Nummerpladen på motorcyklen.
+        /// </summary>
+        public string Licenseplate { get; set; }
+
+        /// <summary>
+        /// Dato for krydsning.
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Returnerer den faste pris for at krydse broen.
+        /// </summary>
+        /// <returns>Prisen som en double (125 DKK).</returns>
+        public double Price()
+        {
+            return 125.0;
+        }
+
+        /// <summary>
+        /// Returnerer køretøjstypen som en streng.
+        /// </summary>
+        /// <returns>En streng, der repræsenterer køretøjstypen.</returns>
+        public string VehicleType()
+        {
+            return "MC";
+        }
     }
 }
+
