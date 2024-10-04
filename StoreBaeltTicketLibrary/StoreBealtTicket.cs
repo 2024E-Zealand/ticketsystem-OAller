2ef0eaecbb71e10c3260b6593b32a311ba6077ba
Oliver Aller <ola001@edu.zealand.dk>
2024-10-04 12:46:40 +0200

    Afsluttet StoreBaeltTicketLibrary med weekend rabat og enhedstests

---
+++ StoreBaeltTicketLibrary/StoreBealtTicket.cs
@@ -0,0 +1,28 @@
+﻿using TicketClassLibrary.Model;
+using System;
+
+namespace StoreBaeltTicketLibrary
+{
+    /// <summary>
+    /// Specifik billet til Storebæltsbroen med weekendrabat.
+    /// </summary>
+    public class StoreBaeltTicket : Car
+    {
+        /// <summary>
+        /// Returnerer prisen for at krydse Storebæltsbroen med specifikke rabatter.
+        /// </summary>
+        /// <returns>Prisen som en double.</returns>
+        public override double Price()
+        {
+            double basePrice = base.Price();
+
+            // Weekend rabat på 20% for Storebæltsbroen
+            if (Date.DayOfWeek == DayOfWeek.Saturday || Date.DayOfWeek == DayOfWeek.Sunday)
+            {
+                return basePrice * 0.8; // 20% rabat i weekenden
+            }
+
+            return basePrice;
+        }
+    }
+}
---
+++ TicketClassLibary.MSTests/StoreBealtTicketTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StoreBaeltTicketLibrary;
+using System;
+
+namespace TicketClassLibrary.MSTests
+{
+    [TestClass]
+    public class StoreBaeltTicketTests
+    {
+        [TestMethod]
+        public void Price_ShouldApplyWeekendDiscount()
+        {
+            // Arrange
+            var ticket = new StoreBaeltTicket
+            {
+                Date = new DateTime(2024, 10, 5) // En lørdag
+            };
+
+            // Act
+            double price = ticket.Price();
+
+            // Assert
+            Assert.AreEqual(240.0 * 0.8, price, 0.01);
+        }
+
+        [TestMethod]
+        public void Price_ShouldNotApplyDiscountOnWeekdays()
+        {
+            // Arrange
+            var ticket = new StoreBaeltTicket
+            {
+                Date = new DateTime(2024, 10, 2) // En onsdag
+            };
+
+            // Act
+            double price = ticket.Price();
+
+            // Assert
+            Assert.AreEqual(240.0, price, 0.01);
+        }
+    }
+}
